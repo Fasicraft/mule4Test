5c12d02ec8170c69d963c23589f04e142d77c20b
Fasika <fsemagn@gmail.com>
2022-03-23 00:43:44 -0400

    test

---
+++ eclipse-workspace/Craft C/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Fasika
+ *
+ */
+module hello {
+}
---
+++ eclipse-workspace/hello/src/hello/hello.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		double a;
+		double b;
+				
+	
+		System.out.print("Enter the first number" + " ");
+		a = input.nextDouble();
+		System.out.print("Enter the second number" + " ");
+		a = input.nextDouble();
+		
+
+	}
+
+}
